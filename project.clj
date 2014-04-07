7f0ed2cfe4416ac3f9ff0c8f0a408fd8928166a4
Alex Kalderimis <alexkalderimis@gmail.com>
2014-04-08 00:54:25 +0100

    Added some comments about what each dep is for.

--- project.clj
+++ project.clj
@@ -2,12 +2,13 @@
   :description "The application holding data-flow steps."
   :url "http://steps.intermine.org"
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [compojure "1.1.6"]
-                 [ring/ring-json "0.1.2"]
-                 [c3p0/c3p0 "0.9.1.2"]
-                 [org.clojure/java.jdbc "0.2.3"]
-                 [com.h2database/h2 "1.3.168"]
-                 [cheshire "4.0.3"]]
+                 [compojure "1.1.6"] ;; Request handlers
+                 [ring/ring-json "0.1.2"] ;; JSON marshalling
+                 [c3p0/c3p0 "0.9.1.2"] ;; DB pooling
+                 [com.stuartsierra/component "0.2.1"] ;; Dependency management
+                 [org.clojure/java.jdbc "0.2.3"] ;; DB interface
+                 [com.h2database/h2 "1.3.168"]   ;; DB Driver
+                 [cheshire "4.0.3"]] ;; JSON serialisation
   :plugins [[lein-ring "0.8.10"]]
   :ring {:handler staircase.handler/app}
   :profiles
