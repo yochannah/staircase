eea43784877282ff470096e7353eb9a53c69141f
Alex Kalderimis <alexkalderimis@gmail.com>
2014-04-09 02:00:33 +0100

    Managed to not include dependency and the tests still ran locally.

--- project.clj
+++ project.clj
@@ -6,6 +6,7 @@
                  [org.clojure/tools.reader "0.8.4"] ;; Read edn
                  [compojure "1.1.6"] ;; Request handlers
                  [ring/ring-json "0.1.2"] ;; JSON marshalling
+                 [ring-mock "0.1.5"] ;; For testing request handlers.
                  [c3p0/c3p0 "0.9.1.2"] ;; DB pooling
                  [com.stuartsierra/component "0.2.1"] ;; Dependency management
                  [honeysql "0.4.3"] ;; SQL sugar
