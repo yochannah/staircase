9c4e9161ea7aa5ef8eaf65eb0d2aeb858618fdea
Alex Kalderimis <alexkalderimis@gmail.com>
2014-05-14 18:42:38 +0100

    add bail on no npm

--- start.sh
+++ start.sh
@@ -5,7 +5,12 @@ set -e
 BOWER=$(which bower)
 NPM=$(which npm)
 
+
 if test -z "$BOWER"; then
+	if test -z "$NPM"; then 
+		echo "npm not found. Cannot install bower" >> error.log
+		exit 1
+	fi
 	echo "Using local bower"
 	$NPM install bower
 	./node_modules/bower/bin/bower install 
