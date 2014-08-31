aaa0c7174bbb0872115284f09744f5ea6ed6aa85
Alex Kalderimis <alexkalderimis@gmail.com>
2014-08-31 22:25:52 +0100

    Improve placement of cookie message.

--- src/clojure/staircase/views/footer.clj
+++ src/clojure/staircase/views/footer.clj
@@ -4,17 +4,6 @@
 
 (defn snippet [config]
   [:section.footer.dark {:ng-controller "FooterCtrl"}
-   [:div.panel.panel-info {:ng-show "showCookieMessage"}
-    [:div.panel-heading "Cookies"]
-    [:div.panel-body
-     [:button.btn.btn-warning.pull-right {:ng-click "showCookieMessage = false"} "understood"]
-     [:p
-      "This site uses cookies to provide essential functionality, such as remembering your
-      identity. You can find out details of what information we store here "
-      (link-to "/cookies" "here")
-      ". By dismissing this message you agree to let this application store the data it needs
-      to operate."]
-     ]]
    [:div.row
     [:div.col-sm-8.site-map
      [:ul
@@ -25,4 +14,15 @@
      (unordered-list (for [[icon addr text] (:contacts config)]
                        (link-to addr [:i.fa.fa-fw.fa-2x {:class icon}] " " text)))]
     ]
+   [:div.row.panel.panel-info {:ng-show "showCookieMessage"}
+    [:div.panel-heading "Cookies"]
+    [:div.panel-body
+     [:button.btn.btn-warning.pull-right {:ng-click "showCookieMessage = false"} "understood"]
+     [:p
+      "This site uses cookies to provide essential functionality, such as remembering your
+      identity. You can find out details of what information we store here "
+      (link-to "/cookies" "here")
+      ". By dismissing this message you agree to let this application store the data it needs
+      to operate."]
+     ]]
    ])
