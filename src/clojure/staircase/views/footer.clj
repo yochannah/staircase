c466420f5b577117517bab6c1a317e2897e0eb11
Alex Kalderimis <alexkalderimis@gmail.com>
2015-03-10 01:43:16 +0000

    Better cookie message

--- src/clojure/staircase/views/footer.clj
+++ src/clojure/staircase/views/footer.clj
@@ -21,15 +21,17 @@
      (unordered-list (for [[icon addr text] (:contacts config)]
                        (link-to addr [:i.fa.fa-fw.fa-2x {:class icon}] " " text)))]
     ]
-   [:div.row.panel.panel-info {:ng-show "showCookieMessage"}
+   [:div#cookie-message.row.panel.panel-info {:ng-show "showCookieMessage"}
     [:div.panel-heading "Cookies"]
-    [:div.panel-body
-     [:button.btn.btn-warning.pull-right {:ng-click "showCookieMessage = false"} "understood"]
-     [:p
-      "This site uses cookies to provide essential functionality, such as remembering your
-      identity. You can find out details of what information we store here "
-      (link-to "/cookies" "here")
-      ". By dismissing this message you agree to let this application store the data it needs
-      to operate."]
-     ]]
+      [:div.panel-body.flex-row.guttered
+        [:div.flex-box.fit
+          [:p
+            "This site uses cookies to provide essential functionality, such as remembering your
+            identity. You can find out details of what information we store here "
+            (link-to "/cookies" "here")
+            ". By dismissing this message you agree to let this application store the data it needs
+            to operate."]]
+        [:div.flex-box
+          [:button.btn.btn-warning.pull-right {:ng-click "showCookieMessage = false"} "understood"]]
+      ]]
    ])
