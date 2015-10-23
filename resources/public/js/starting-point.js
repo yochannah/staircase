52b37f12a969538cdf1e7c0f13708dc0ac06b58d
Joshua Heimbach <joshkh@gmail.com>
2015-10-23 13:01:18 +0100

    Remove generated files.

--- resources/public/js/starting-point.js
+++
@@ -1,9 +0,0 @@
-(function() {
-  define(function(require) {
-    var ng;
-    ng = require('angular');
-    require('starting-point/controller');
-    return ng.module('steps.starting-point', ['steps.starting-point.controller']);
-  });
-
-}).call(this);
